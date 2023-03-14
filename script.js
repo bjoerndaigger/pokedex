7ffa4ca23dc0aab83f14a3682a56b015633c4f65
bjoerndaigger <bjoern.daigger@gmail.com>
2023-03-14 19:25:09 +0100

    render images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let pokemonData = []; // globale Variable die JSON von API aufruft
 
 async function loadPokemon() {
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i < 5; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}/`; // URL der API
         let response = await fetch(url); // Aufruf an Server und Abruf der Daten
         let pokemon = await response.json(); // umwandeln der Textdaten ins JSON-Format
@@ -11,8 +11,18 @@ async function loadPokemon() {
 }
 
 function renderPokemon() {
-   document.getElementById('pokemon').innerHTML = `Test`;
+    let content = document.getElementById('pokemon');
+    content.innerHTML = '';
 
+    for (let i = 0; i < pokemonData.length; i++) {
+        const pokemonName = pokemonData[i];
+        content.innerHTML += `
+            <div>
+                <h2 class="text-capitalize">${pokemonName['name']}</h2>
+                <img class="pokemon-img" src="${pokemonName['sprites']['other']['official-artwork']['front_default']}">
+            </div>
+        `;
+    }
 }
 
 
