a2261188133063594be966bfa3a7c2604b901e71
bjoerndaigger <bjoern.daigger@gmail.com>
2023-03-14 13:21:55 +0100

    render names

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let currentPokemon; // globale Variable die JSON von API aufruft
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander'; // URL der API
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'; // URL der API
     let response = await fetch(url); // Aufruf an Server und Abruf der Daten
     currentPokemon = await response.json(); // umwandeln der Textdaten ins JSON-Format
     console.log('loaded Pokemon', currentPokemon);
     renderPokemon();
-    renderPokemonCard();
+   // renderPokemonCard();
 }
 
 function renderPokemon() {
+ let container = document.getElementById('pokemon');
+ let pokemon = currentPokemon['results'];
+ 
+for (let i = 0; i < pokemon.length; i++) {
+    const names = pokemon[i];
+    let name = names['name']
+    container.innerHTML += `
+    <div>${name}</div>
+    `;
+    
+}
+  
+}
+
+/* function renderPokemon() {
     let name = currentPokemon['name'];
     let img = currentPokemon['sprites']['other']['official-artwork']['front_default'];
 
@@ -17,9 +32,9 @@ function renderPokemon() {
         <h2>${name}</h2>
         <img class="pokemon-img" src="${img}">
     `;
+}*/
 
-}
-function renderPokemonCard() {
+/*function renderPokemonCard() {
     document.getElementById('pokemon-name').innerHTML = currentPokemon['name']; // rendern des Namens
     document.getElementById('pokemon-img').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-}
+}*/
