25cf7bc61419a7ef1ec2b8c3726aba032caf43ff
bjoerndaigger <bjoern.daigger@gmail.com>
2023-03-20 11:20:18 +0100

    for-loop types

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ async function loadPokemon() {
         let pokemon = await response.json(); // umwandeln der Textdaten ins JSON-Format
         pokemonData.push(pokemon); // push der Daten in Array pokemonData
         renderPokemon();
+        renderPokemonCard();
     }
 }
 
@@ -16,14 +17,14 @@ function renderPokemon() {
 
     for (let i = 0; i < pokemonData.length; i++) {
         const pokemonName = pokemonData[i];
-        
+
         let name = pokemonName['name'];
         let id = pokemonName['id'];
         let idAsString = id.toString().padStart(3, '0');
         let image = pokemonName['sprites']['other']['official-artwork']['front_default'];
         let type = pokemonName['types'][0]['type']['name'];
-       
-       
+
+
         content.innerHTML += /*html*/ `
             <div id="frame${i}" class="pokemon-container text-white m-3 p-3 rounded-5"> 
                 <div class="d-flex justify-content-between">
@@ -31,30 +32,36 @@ function renderPokemon() {
                     <small class="fw-bold">#${idAsString}</small>
                 </div>
                 <div class="d-flex flex-row justify-content-between">
-                    <div>
-                        <div class="text-capitalize type-field rounded-5">${type}</div>
-                    </div>
+                    <div id="pokemon-types${i}"></div>
                     <img class="pokemon-img" src="${image}"> 
                 </div>
             </div>
         `;
         chooseBackgroundColor(i, type);
+        renderPokemonTypes(i, pokemonName);
     }
 }
 
-// To-Do: Render Pokemon-Type
+
+function renderPokemonTypes(i, pokemonName) {
+    let content = document.getElementById(`pokemon-types${i}`);
+    for (let i = 0; i < pokemonName['types'].length; i++) {
+        const types = pokemonName['types'][i]['type']['name'];
+
+        content.innerHTML += `
+        <div class="text-capitalize type-field rounded-5 mb-1">${types}</div>
+    `;
+    }
+}
+
 
 function chooseBackgroundColor(i, type) {
     document.getElementById(`frame${i}`).classList.add(type);
 
 }
 
-function loadMorePokemon() {
-    
+
+function renderPokemonCard() {
+    document.getElementById('pokemon-name').innerHTML = 'Test';
 }
 
-
-/* function renderPokemonCard() {
-    document.getElementById('pokemon-name').innerHTML = pokemonData['name']; // rendern des Namens
-    document.getElementById('pokemon-img').src = pokemonData['sprites']['other']['official-artwork']['front_default'];
-}*/
