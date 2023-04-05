b77c8495d50d131423eeb1c70c934b3afbd03697
bjoerndaigger <bjoern.daigger@gmail.com>
2023-04-05 14:24:41 +0200

    render search content check

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function loadPokemon() {
         let response = await fetch(url); // Aufruf an Server und Abruf der Daten
         let pokemon = await response.json(); // umwandeln der Textdaten ins JSON-Format
         pokemonData.push(pokemon); // push der Daten in Array pokemonData
-        renderPokemon(pokemonData, false);
+        renderPokemon(pokemonData, false);  // render komplette Pokemon Liste, wenn Search-Input leer ist
     }
 }
 
@@ -113,24 +113,22 @@ function openPopupCard(i, search) {
 
 // If-Else, which renders all Pokemon or search results
 function contentPopupCard(i, search) {
-    if (!search) {
+    if (search === false) {
         let currentPokemon = pokemonData[i];
-        document.getElementById('card-container').innerHTML = htmlOpenPopupCard(i, currentPokemon);
+        document.getElementById('card-container').innerHTML = htmlOpenPopupCard(i, currentPokemon, search);
         renderTypesPopupCard(i, currentPokemon);
         getBackgroundColorPopupCard(i, currentPokemon);
-        showAbout(i, currentPokemon);
-        console.log(i, currentPokemon, search);
+        showAbout(i, search);
     } else {
         let currentPokemon = selectedPokemon[i];
-        document.getElementById('card-container').innerHTML = htmlOpenPopupCard(i, currentPokemon);
+        document.getElementById('card-container').innerHTML = htmlOpenPopupCard(i, currentPokemon, search);
         renderTypesPopupCard(i, currentPokemon);
         getBackgroundColorPopupCard(i, currentPokemon);
-        showAbout(i, currentPokemon);
-        console.log(i, currentPokemon, search);
+        showAbout(i, search);
     }
 }
 
-function htmlOpenPopupCard(i, currentPokemon) {
+function htmlOpenPopupCard(i, currentPokemon, search) {
     let name = currentPokemon['name'];
     let id = currentPokemon['id'];
     let idAsString = id.toString().padStart(3, '0');
@@ -154,8 +152,8 @@ function htmlOpenPopupCard(i, currentPokemon) {
             <img class="pokemon-card-img" src="${image}">
 
             <div class="d-flex justify-content-between w-50 p-2"> 
-                <a id="pokemon-card-nav-link-1" class="pokemon-card-nav text-secondary fw-bold" href="javascript:void(0);" onclick="showAbout(${i})">About</a>       
-                <a id="pokemon-card-nav-link-2" class="pokemon-card-nav text-secondary fw-bold" href="javascript:void(0);" onclick="showStats(${i})">Stats</a> 
+                <a id="pokemon-card-nav-link-1" class="pokemon-card-nav text-secondary fw-bold" href="javascript:void(0);" onclick="showAbout(${i}, ${search})">About</a>       
+                <a id="pokemon-card-nav-link-2" class="pokemon-card-nav text-secondary fw-bold" href="javascript:void(0);" onclick="showStats(${i}, ${search})">Stats</a> 
             </div>
             <div id="more-details" class="w-100">
             </div>
@@ -177,46 +175,60 @@ function renderTypesPopupCard(i, currentPokemon) {
 }
 
 
-function showAbout(i) {
+function showAbout(i, search) {
     document.getElementById('pokemon-card-nav-link-2').classList.remove('text-dark');
     document.getElementById('pokemon-card-nav-link-1').classList.add('text-dark');
 
     let content = document.getElementById('more-details');
     content.innerHTML = '';
 
-    let height = pokemonData[i]['height'];
+    if (search === false) {
+        let currentPokemon = pokemonData[i];
+        content.innerHTML += htmlShowAbout(currentPokemon);
+        getAbilities(currentPokemon);
+    } else {
+        let currentPokemon = selectedPokemon[i];
+        content.innerHTML += htmlShowAbout(currentPokemon);
+        getAbilities(currentPokemon);
+    }
+
+   
+}
+
+function htmlShowAbout(currentPokemon) {
+
+    let height = currentPokemon['height'];
     height = height / 10;
     height = height.toString().replace('.', ',');
-    let weight = pokemonData[i]['weight'];
+    let weight = currentPokemon['weight'];
     weight = weight / 10;
     weight = weight.toString().replace('.', ',');
 
-    content.innerHTML += /*html*/`
-        <table class="table fw-bold">
-	        <tr>
-		        <td class="text-secondary">Height</td>
-		        <td>${height} m</td>				
-	        </tr>
-            <tr>
-		        <td class="text-secondary">Weight</td>
-		        <td>${weight} kg</td>				
-	        </tr>
-            <tr>
-		        <td class="text-secondary">Abilities</td>
-		        <td id="abilities" class="text-capitalize"></td>				
-	        </tr>
-        </table>
-    `;
-    getAbilities(i);
+    return /*html*/`
+    <table class="table fw-bold">
+        <tr>
+            <td class="text-secondary">Height</td>
+            <td>${height} m</td>				
+        </tr>
+        <tr>
+            <td class="text-secondary">Weight</td>
+            <td>${weight} kg</td>				
+        </tr>
+        <tr>
+            <td class="text-secondary">Abilities</td>
+            <td id="abilities" class="text-capitalize"></td>				
+        </tr>
+    </table>
+`;
 }
 
 
-function getAbilities(i) {
+function getAbilities(currentPokemon) {
     let content = document.getElementById('abilities');
     content.innerHTML = '';
 
-    for (let j = 0; j < pokemonData[i]['abilities'].length; j++) {
-        const abilities = pokemonData[i]['abilities'][j]['ability']['name'];
+    for (let j = 0; j < currentPokemon['abilities'].length; j++) {
+        const abilities = currentPokemon['abilities'][j]['ability']['name'];
 
         content.innerHTML += `
             <div>${abilities}</div>
@@ -227,74 +239,85 @@ function getAbilities(i) {
 }
 
 
-function showStats(i, currentPokemon) {
+function showStats(i, search) {
     document.getElementById('pokemon-card-nav-link-1').classList.remove('text-dark');
     document.getElementById('pokemon-card-nav-link-2').classList.add('text-dark');
-
-    let hp = pokemonData[i]['stats'][0]['base_stat'];
-    let attack = pokemonData[i]['stats'][1]['base_stat'];
-    let defense = pokemonData[i]['stats'][2]['base_stat'];
-    let specialAttack = pokemonData[i]['stats'][3]['base_stat'];
-    let specialDefense = pokemonData[i]['stats'][4]['base_stat'];
-    let speed = pokemonData[i]['stats'][5]['base_stat'];
 
     let content = document.getElementById('more-details');
     content.innerHTML = '';
 
-    content.innerHTML += /*html*/`
-        <table class="table">
-            <tr>
-                <td class="text-secondary small">HP</td>
-                <td>
-                    <div class="progress">
-                        <div class="progress-bar bg-danger" role="progressbar" style="width: ${hp / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${hp}</div>
-                    </div>
-                </td>				
-            </tr>
-            <tr>
-                <td class="text-secondary small">Attack</td>
-                <td>
-                    <div class="progress">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: ${attack / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${attack}</div>
-                    </div>
-                </td>				
-            </tr>
-            <tr>
-                <td class="text-secondary small">Defense</td>
-                <td>
-                    <div class="progress">
-                        <div class="progress-bar bg-danger" role="progressbar" style="width: ${defense / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${defense}</div>
-                    </div>
-                </td>				
-            </tr>
-            <tr>
-                <td class="text-secondary small">Sp. Atk</td>
-                <td>
-                    <div class="progress">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: ${specialAttack / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${specialAttack}</div>
-                    </div>
-                </td>				
-            </tr>
-            <tr>
-                <td class="text-secondary small">Sp. Def</td>
-                <td>
-                    <div class="progress">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: ${specialDefense / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${specialDefense}</div>
-                    </div>
-                </td>				
-            </tr>
-            <tr>
-                <td class="text-secondary small">Speed</td>
-                <td>
-                    <div class="progress">
-                        <div class="progress-bar bg-danger" role="progressbar" style="width: ${speed / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${speed}</div>
-                    </div>
-                </td>				
-            </tr>
-        </table>
-    `;
+    if (search === false) {
+        let currentPokemon = pokemonData[i];
+        content.innerHTML += htmlShowStats(currentPokemon);
+       
+    } else {
+        let currentPokemon = selectedPokemon[i];
+        content.innerHTML += htmlShowStats(currentPokemon);
+    }
 }
 
+
+function htmlShowStats(currentPokemon) {
+    let hp = currentPokemon['stats'][0]['base_stat'];
+    let attack = currentPokemon['stats'][1]['base_stat'];
+    let defense = currentPokemon['stats'][2]['base_stat'];
+    let specialAttack = currentPokemon['stats'][3]['base_stat'];
+    let specialDefense = currentPokemon['stats'][4]['base_stat'];
+    let speed = currentPokemon['stats'][5]['base_stat'];
+
+    return /*html*/`
+    <table class="table">
+        <tr>
+            <td class="text-secondary small">HP</td>
+            <td>
+                <div class="progress">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: ${hp / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${hp}</div>
+                </div>
+            </td>				
+        </tr>
+        <tr>
+            <td class="text-secondary small">Attack</td>
+            <td>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: ${attack / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${attack}</div>
+                </div>
+            </td>				
+        </tr>
+        <tr>
+            <td class="text-secondary small">Defense</td>
+            <td>
+                <div class="progress">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: ${defense / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${defense}</div>
+                </div>
+            </td>				
+        </tr>
+        <tr>
+            <td class="text-secondary small">Sp. Atk</td>
+            <td>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: ${specialAttack / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${specialAttack}</div>
+                </div>
+            </td>				
+        </tr>
+        <tr>
+            <td class="text-secondary small">Sp. Def</td>
+            <td>
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: ${specialDefense / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${specialDefense}</div>
+                </div>
+            </td>				
+        </tr>
+        <tr>
+            <td class="text-secondary small">Speed</td>
+            <td>
+                <div class="progress">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: ${speed / 150 * 100}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${speed}</div>
+                </div>
+            </td>				
+        </tr>
+    </table>
+`;
+}
 
 function getBackgroundColorPopupCard(i, currentPokemon) {
     let color = currentPokemon['types'][0]['type']['name'];
@@ -302,7 +325,7 @@ function getBackgroundColorPopupCard(i, currentPokemon) {
 }
 
 
-function previousPokemon(i) {
+/* function previousPokemon(i) {
     if (i == 0) {
         document.getElementById('card-container').innerHTML = ``;
         i = pokemonData.length - 1;
@@ -326,7 +349,7 @@ function nextPokemon(i) {
         document.getElementById('card-container').innerHTML = '';
         openPopupCard(i);
     }
-}
+} */
 
 
 function closePopupCard() {
